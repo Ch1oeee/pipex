0f2a2407b3e87b9c2014e60dd36ed453d51ba753
Chloe Montaigut <cmontaig@c1r4p4.42nice.fr>
2025-03-07 15:45:29 +0100

    initialize repo

---
+++ Makefile
@@ -0,0 +1,44 @@
+NAME = pipex
+FLAGS = -Wall -Wextra -Werror #-g3 -fsanitize=address
+RM = rm -rf
+
+GREEN = \033[38;5;114m
+ROSE = \033[38;5;218m
+BLUE = \033[38;5;153m
+NC = \033[0m
+
+LIBRARIES_DIR = ./Libraries/libft
+LIBRARIES = $(LIBRARIES_DIR)/libft.a
+
+SRCS =	
+
+OBJS = $(SRCS:.c=.o)
+
+$(NAME): $(LIBRARIES) $(OBJS)
+	@echo "$(ROSE)\e[1m┌─────$(NAME)────────────────────────────────────┐\e"
+	@echo "││$(BLUE)	      Compiling $(NAME) 🌀		$(ROSE) │"
+	@echo "\e[1m└────────────────────────────────────────────────┘\e"
+	@$(CC) $(FLAGS) -o $(NAME) $(OBJS) $(LIBRARIES)
+	@echo "$(RED)\e[1m┌─────$(NAME)────────────────────────────────────┐\e"
+	@echo "││$(GREEN)	      \e[1mCompilation finished 🌱\e		$(ROSE) │"
+	@echo "\e[1m└────────────────────────────────────────────────┘\e"
+
+$(LIBRARIES):
+	@$(MAKE) -C $(LIBRARIES_DIR) --no-print-directory
+
+.c.o:
+	@cc $(FLAGS) -c -o $@ $<
+
+all: $(NAME)
+
+clean:
+	@$(RM) $(OBJS) 
+	@$(MAKE) -C $(LIBRARIES_DIR) clean --no-print-directory
+
+fclean: clean
+	@$(RM) $(NAME)
+	@$(MAKE) -C $(LIBRARIES_DIR) fclean --no-print-directory
+
+re: fclean all
+
+.PHONY: all bonus clean fclean re 
---
+++ srcs/pipex.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmontaig <cmontaig@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/05 16:03:20 by cmontaig          #+#    #+#             */
+/*   Updated: 2025/03/07 15:33:36 by cmontaig         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include "../Libraries/libft/libft.h"
+# include "../Libraries/ft_printf/ft_printf.h"
+
+typedef struct pipex
+{
+	char	**env;
+};
+
+
+
+#endif
